0f99924d16ba912543a287e1bd403a9343fe0375
DESKTOP-PSJPLKQ\conta <paolo.barcellos@saludigital.com.br>
2022-07-20 14:56:57 -0300

    att git

---
+++ test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="/estilos.css">
+</head>
+
+<body>
+</body>
